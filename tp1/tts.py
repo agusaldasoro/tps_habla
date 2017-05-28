6f29e48f369458eb8332c77a7b4bb27f833d3f0f
Agustina Aldasoro <agusaldasoro@gmail.com>
2017-05-28 12:40:35 -0300

    Quilombo

--- tp1/tts.py
+++ tp1/tts.py
@@ -2,6 +2,80 @@ import os
 import sys
 import math
 from shutil import copyfile
+from shutil import rmtree
+
+def main():
+    phrase = sys.argv[1]
+    fileName = sys.argv[2]
+
+    prosodia = False
+    if phrase[-1:] == '?':
+        prosodia = True
+        phrase = phrase[:-1]
+
+    listOfDiphones = obtainDiphones(phrase)
+    diphones = buildWavFiles(listOfDiphones, 'diphones/')
+
+    buildPraatScrip(diphones, 'concatenate.praat', fileName)
+    # if not os.path.exists(fileName):
+    #     os.makedirs(fileName)
+    os.system('praat concatenate.praat')
+
+    if prosodia:
+        print('praat extract-pitch-track.praat ' + fileName + ' ' + fileName[:-4] + '.PitchTier 50 300')
+        os.system('praat extract-pitch-track.praat ' + fileName + ' ' + fileName[:-4] + '.PitchTier 50 300')
+        # convertIntoQuestion(fileName[:-4])
+        # os.system('praat reemplazar-pitch-track.praat ' + fileName + ' ' + fileName[:-4] + '-mod.PitchTier ' + fileName[:-4] + '-mod.wav 50 500')
+        # copyfile( fileName[:-4] + '-mod.wav',fileName)
+
+    garbageCollector()
+
+
+def obtainDiphones(aString):
+    '''gets diphones from an input strings'''
+    aList = []
+    aString = '-' + aString + '-'
+    for i in range(0, len(aString)-1):
+        aList.append(aString[i] + aString[i+1])
+
+    return aList
+
+
+def buildWavFiles(aListOfDiphones, aWavDirectory):
+    '''Copy and rename the necesary wav files to tempWavs'''
+    if not os.path.exists('tempWavs/'):
+        os.makedirs('tempWavs/')
+
+    files = [ a + '.wav' for a in aListOfDiphones ]
+    i = 0
+    diphones = []
+
+    for aFile in files:
+        copyfile(aWavDirectory + aFile, 'tempWavs/diphone' + str(i) + '.wav')
+        diphones.append('tempWavs/diphone' + str(i) + '.wav')
+        i += 1
+
+    return diphones
+
+
+def buildPraatScrip(listOfWavFiles, aPraatFile, anOutputFile):
+    '''write and executes a praat script that'll concatenate the wav files
+       specified on listOfWavFiles on outputFile'''
+
+    with open(aPraatFile, 'w') as f:
+        currentDir = os.getcwd()
+        for aFile in listOfWavFiles:
+            f.write('Read from file... ' + currentDir + '/' + aFile + '\n')
+
+        f.write('select Sound diphone0'+ '\n')
+        for i in range(1,len(listOfWavFiles)):
+            f.write('plus Sound diphone' + str(i) + '\n')
+
+        f.write('Concatenate recoverably\n')
+        f.write('select Sound chain\n')
+        f.write('Save as WAV file... ' + currentDir + anOutputFile)
+
+
 
 def parsePitchTier(aPitchTier):
 
@@ -23,6 +97,9 @@ def parsePitchTier(aPitchTier):
 
 def builPitchTier(aPitchTier, xmin, xmax,points):
 
+    if not os.path.exists(aPitchTier):
+        os.makedirs(aPitchTier)
+
     with open(aPitchTier, 'w') as f:
         f.write('File type = "ooTextFile"\n')
         f.write('Object class = "PitchTier"\n')
@@ -38,47 +115,11 @@ def builPitchTier(aPitchTier, xmin, xmax,points):
 
 
 
-def obtainDiphones(aString):
-    '''gets diphones from an input strings
-    '''
-    aList = []
-    dummyString = '-' + aString + '-'
-    for i in range(0, len(dummyString)-1):
-        aList.append(dummyString[i] + dummyString[i+1])
-
-    return aList
-
-
-def buildWavFiles(aListOfDiphones, aWavDirectory):
-    '''Copy and rename the necesary wav files to tempWavs
-    '''
-    files = [ a + '.wav' for a in aListOfDiphones]
-    i = 0
-    diphones = []
-    for f in files:
-        copyfile(aWavDirectory + f, 'tempWavs/diphone' + str(i) + '.wav')
-        diphones.append('tempWavs/diphone' + str(i) + '.wav')
-        i += 1
-    return diphones
-
-def buildPraatScrip(listOfWavFiles, aPraatFile, anOutputFile):
-    '''write and executes a praat script that'll concatenate the wav files specified on listOfWavFiles on outputFile
-    '''
-    with open(aPraatFile, 'w') as f:
-        currentDir = os.getcwd()
-        for aFile in listOfWavFiles:
-            f.write('Read from file... '+currentDir +'/' + aFile + '\n')
-        f.write('select Sound diphone0'+ '\n')
-        for i in range(1,len(listOfWavFiles)):
-            f.write('plus Sound diphone' + str(i) + '\n')
-
-        f.write('Concatenate recoverably\n')
-        f.write('select Sound chain\n')
-        f.write('Save as WAV file... '+currentDir + '/' + anOutputFile)
 
 
 
-def transformarEnPregunta(fileName):
+
+def convertIntoQuestion(fileName):
 
     originalPitchTier = fileName + '.PitchTier'
     newPitchTier = fileName + '-mod.PitchTier'
@@ -94,25 +135,10 @@ def transformarEnPregunta(fileName):
 
     builPitchTier(newPitchTier, xmin, xmax, newPoints)
 
+def garbageCollector():
+    rmtree('tempWavs/')
 
-def main():
 
-    prosodia = False
-    a = sys.argv[1]
-    if a[-1:] == '?':
-        prosodia = True
-        a = a[:-1]
-
-    fileName = sys.argv[2]
-    listOfDiphones = obtainDiphones(a)
-    diphones = buildWavFiles(listOfDiphones, 'difonos2/')
-    buildPraatScrip(diphones, 'concatenate.praat', fileName)
-    os.system('praat concatenate.praat')
-    if prosodia:
-        os.system('praat extraer-pitch-track.praat ' + fileName + ' ' + fileName[:-4] + '.PitchTier 50 300')
-        transformarEnPregunta(fileName[:-4])
-        os.system('praat reemplazar-pitch-track.praat ' + fileName + ' ' + fileName[:-4] + '-mod.PitchTier ' + fileName[:-4] + '-mod.wav 50 500')
-        copyfile( fileName[:-4] + '-mod.wav',fileName)
 
 if __name__ == '__main__':
     main()
